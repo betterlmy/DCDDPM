fc3e4f7b142cab1c046d46d2d38a641565ac2488
Zane <betterlmy@icloud.com>
2023-11-14 11:21:53 +0800

    Initial commit

---
+++ model.py
@@ -0,0 +1,55 @@
+from DCUnet import *
+import torch.nn.functional as F
+
+
+class DCDDPM(nn.Module):
+    def __init__(self, unet_model, beta_schedule="linear", T=1000):
+        super(DCDDPM, self).__init__()
+        self.seed = 1
+        self.unet = unet_model
+        self.T = T  # Number of diffusion steps
+        self.beta_schedule = self._get_beta_schedule(beta_schedule, T)
+
+    def _get_beta_schedule(self, schedule, T):
+        if schedule == "linear":
+            return torch.linspace(0.0001, 0.02, T)
+        else:
+            raise ValueError("Unknown beta schedule")
+
+    def control_seed(self):
+        torch.manual_seed(self.seed)
+        self.seed += 1
+
+    def add_noise_sample(self, x_start, t, noise=None):
+        """ Sample from q(x_t | x_0) """
+        self.control_seed()
+        if noise is None:
+            noise = torch.randn_like(x_start)
+        beta_t = self.beta_schedule[t].unsqueeze(1).unsqueeze(2).unsqueeze(3)
+        return torch.sqrt(1 - beta_t) * x_start + torch.sqrt(beta_t) * noise, torch.sqrt(beta_t) * noise
+
+    # def p_sample(self, x_t, t):
+    #     """ Sample from p(x_{t-1} | x_t) """
+    #     if torch.all(t) == 0:
+    #         return x_t
+    #     else:
+    #         beta_t = self.beta_schedule[t]
+    #         pred_noise = self.unet(x_t, t)
+    #         mean = (1 / torch.sqrt(1 - beta_t)) * (x_t - (beta_t / torch.sqrt(1 - beta_t)) * pred_noise)
+    #         noise = torch.randn_like(x_t)
+    #         return mean + torch.sqrt(beta_t) * noise
+
+    def get_loss(self, x_t, scaled_noise, t):
+        """ Get loss for training """
+        self.control_seed()
+
+        pred_noise = self.unet(x_t)  # self.unet(x_t, t)
+        loss = F.mse_loss(pred_noise, scaled_noise, reduction="none")
+        # loss = torch.mean((x_recon - x_start) ** 2)
+        return loss
+
+    def forward(self, x_start):
+        self.control_seed()
+        t = torch.randint(0, self.T, (x_start.size(0),), device=x_start.device)
+        x_t, scaled_noise = self.add_noise_sample(x_start, t)
+        return self.get_loss(x_t, scaled_noise, t)
---
+++ train.py
@@ -0,0 +1,45 @@
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from dataset_loader import CTImagesDataset
+from model import DCDDPM
+from torchvision import transforms
+from DCUnet import *
+
+# 超参数设置
+learning_rate = 1e-4
+batch_size = 16
+num_epochs = 100
+
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),  # 或其他您需要的大小
+    transforms.ToTensor(),
+])
+high_dir = '/Users/zane/Desktop/B301MM/high'
+low_dir = '/Users/zane/Desktop/B301MM/low'
+
+train_dataset = CTImagesDataset(high_dir, low_dir, transform)
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+# 模型初始化
+unet_model = DualChannelUnet(in_channels=1, out_channels=1)  # Replace with your Unet model
+model = DCDDPM(unet_model)
+
+# 损失函数和优化器
+criterion = nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+# 训练循环
+for epoch in range(num_epochs):
+    for high_img, low_img in train_loader:
+        # 假设模型的输入是高低图像的组合
+        # inputs = torch.cat((high_img, low_img), dim=1)
+        loss = model(low_img)
+
+        # loss = criterion(outputs, high_img)  # 假设目标是重建高图像
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}')
+
+# 保存模型
+torch.save(model.state_dict(), 'ddpm_model.pth')
